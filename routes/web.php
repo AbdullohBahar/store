0aac398dfb76b27545ab9594a2deba7aec2544e5
AbdullohBahar <abdullohbahar@gmail.com>
2021-08-25 18:26:33 +0700

    membuat halaman login dan signup

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\DashboardTokoController;
 use App\Http\Controllers\Landing;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +26,5 @@ Route::get('user', [DashboardTokoController::class, 'dashboardToko'])->name('das
 Route::get('produk', [DashboardTokoController::class, 'produkToko'])->name('produk');
 Route::get('tambah-produk', [DashboardTokoController::class, 'tambahProduk'])->name('tambah-produk');
 Route::get('profile-toko', [DashboardTokoController::class, 'profileToko'])->name('profile-toko');
+Route::get('login', [LoginController::class, 'login'])->name('login');
+Route::get('signup', [SignupController::class, 'signup'])->name('signup');
